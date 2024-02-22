dddc8eec56014127c02a9c1ef23eefbdb9fae335
AlissonRoss <alirosssalazar@gmail.com>
2024-02-21 16:27:50 -0800

    Login API frontend to backend progress

--- src/api-services/AuthService.js
+++ src/api-services/AuthService.js
@@ -2,12 +2,7 @@
 //Author: https://github.com/ConnorBstill/cars-front-end/blob/master/src/ApiServices/AuthService.ts
 import { API_URL } from '../environment';
 
-interface AuthRequestBody {
-  username: string;
-  password: string;
-}
-
-export const register = async (body: AuthRequestBody) => {
+export const register = async (body) => {
   const response = await fetch(`${API_URL}/register`, {
     method: 'POST',
     headers: { 
@@ -21,13 +16,13 @@ export const register = async (body: AuthRequestBody) => {
   return data;
 }
 
-export const LogIn = async (body: AuthRequestBody) => {
+export const LogIn = async (payload) => {
   const response = await fetch(`${API_URL}/login`, {
     method: 'POST',
     headers: { 
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(body)
+    body: JSON.stringify(payload)
   });
 
   const data = await response.json();
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,35 @@
 import { Button, TextField } from "@mui/material";
-import {Link} from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import {LogIn} from '../api-services/AuthService';
+import { useState } from "react";
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    
+    try {
+      const {jwt, success} = await LogIn({ username, password });
+      if(success){
+        setUsername(jwt);
+        localStorage.setItem('blog-app-jwt',jwt);
+        navigate('/blog')
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  };
   return (
     <div className="Login">   
         <div className="Login-container">
             <h1>Login Page for Module 4 Final</h1>
             <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-            <TextField color="secondary" required id="filled-email-input" label="Email" />
+            <TextField color="secondary" required id="filled-email-input" label="Email" 
+            onChange={(e) => setUsername(e.target.value)}/>
             <TextField color="secondary" required id="filled-password-input" label="Password"
-            type="password" autoComplete="current-password" />
-            <Button variant="contained" color="secondary" component={Link} to='/blog'>Sign In</Button>
+            type="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)}/>
+            <Button variant="contained" color="secondary" onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
